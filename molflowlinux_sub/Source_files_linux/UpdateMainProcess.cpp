d6754bc9d2d5ef83b2b274c2d5d94edb8a2acb88
Rudolf Schoenmann <rudolf.schoenmann@tum.de>
2020-04-09 11:39:15 +0200

    introduced the modifactions of 'getStepSize' function also in master branch

--- molflowlinux_sub/Source_files_linux/UpdateMainProcess.cpp
+++ molflowlinux_sub/Source_files_linux/UpdateMainProcess.cpp
@@ -36,23 +36,35 @@ double getStepSize(){
 
 	//Dynamical calculation of min_time is not straight forward, since 'manageTimeStep()' can change it.
 	//Dynamical calculation can be done later, if it is regarded as useful.
-	double t_start = T_min*exp((double)simHistory->currentStep*(log(p->maxTimeS/(T_min))/(double)p->iterationNumber));
-	double t_stop = T_min*exp((double)(simHistory->currentStep+1)*(log(p->maxTimeS/(T_min))/(double)p->iterationNumber));
-	double Delta = t_stop - t_start;
-	double Delta_final = p->t_max - t_start;
-	if(t_stop > p->t_max){
-		return Delta_final;
-	}
-	else{
-		return Delta;
-	}
-	/*if(simHistory->currentStep==0){
-		double T_min = estimateAverageFlightTime();
-		return T_min*exp((double)simHistory->currentStep*(log(p->maxTimeS/T_min)/(double)p->iterationNumber));
+
+		if(simHistory->currentStep == 0){
+			double test_time_step;
+			test_time_step = T_min*(exp((log(p->maxTimeS/(T_min))/(double)p->iterationNumber)) - 1);
+			while(test_time_step < T_min){
+				p->iterationNumber -=1;
+				test_time_step = T_min*(exp((log(p->maxTimeS/(T_min))/(double)p->iterationNumber)) - 1);
+			}
+			return T_min;
 		}
-	else{
-		return exp((double)simHistory->currentStep*(log(p->maxTimeS/simHistory->coveringList.pointintime_list[1].first)/(double)p->iterationNumber));
-		}*/
+		else{
+			double t_start = T_min*exp((double)simHistory->currentStep*(log(p->maxTimeS/(T_min))/(double)p->iterationNumber));
+			double t_stop = T_min*exp((double)(simHistory->currentStep+1)*(log(p->maxTimeS/(T_min))/(double)p->iterationNumber));
+			double Delta = t_stop - t_start;
+			double Delta_final = p->t_max - t_start;
+			if(t_stop > p->t_max){
+				return Delta_final;
+			}
+			else{
+				return Delta;
+			}
+		}
+		/*if(simHistory->currentStep==0){
+			double T_min = estimateAverageFlightTime();
+			return T_min*exp((double)simHistory->currentStep*(log(p->maxTimeS/T_min)/(double)p->iterationNumber));
+			}
+		else{
+			return exp((double)simHistory->currentStep*(log(p->maxTimeS/simHistory->coveringList.pointintime_list[1].first)/(double)p->iterationNumber));
+			}*/
 
 }
 
