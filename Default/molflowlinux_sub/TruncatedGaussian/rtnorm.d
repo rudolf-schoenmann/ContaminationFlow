7947136f0577a37244c88f6f12fdaa32e41d9c0f
Rudolf Schoenmann <rudolf.schoenmann@tum.de>
2020-07-23 13:46:34 +0200

    correct some errors in project definition

---
+++ Default/molflowlinux_sub/TruncatedGaussian/rtnorm.d
@@ -0,0 +1,112 @@
+molflowlinux_sub/TruncatedGaussian/rtnorm.o: \
+ ../molflowlinux_sub/TruncatedGaussian/rtnorm.cpp \
+ /scratch/schoenmann/MolflowLinux/include/include/gsl/gsl_randist.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_rng.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_types.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_errno.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_inline.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_complex_long_double.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_complex.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_check_range.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_long_double.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_long_double.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_complex.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_complex_long_double.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_complex_double.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_double.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_double.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_complex_double.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_complex_float.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_float.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_float.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_complex_float.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_ulong.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_ulong.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_long.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_long.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_uint.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_uint.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_int.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_int.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_ushort.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_ushort.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_short.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_short.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_uchar.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_uchar.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_char.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_char.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_complex_long_double.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_complex_double.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_complex_float.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_long_double.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_double.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_float.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_ulong.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_long.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_uint.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_int.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_ushort.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_short.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_uchar.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_char.h \
+ /scratch/schoenmann/MolflowLinux/include/include/gsl/gsl_sf_erf.h \
+ /scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_sf_result.h \
+ ../molflowlinux_sub/TruncatedGaussian/rtnorm.hpp
+/scratch/schoenmann/MolflowLinux/include/include/gsl/gsl_randist.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_rng.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_types.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_errno.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_inline.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_complex_long_double.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_complex.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_check_range.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_long_double.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_long_double.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_complex.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_complex_long_double.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_complex_double.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_double.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_double.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_complex_double.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_complex_float.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_float.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_float.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_complex_float.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_ulong.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_ulong.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_long.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_long.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_uint.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_uint.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_int.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_int.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_ushort.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_ushort.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_short.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_short.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_uchar.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_uchar.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_vector_char.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_block_char.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_complex_long_double.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_complex_double.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_complex_float.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_long_double.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_double.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_float.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_ulong.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_long.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_uint.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_int.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_ushort.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_short.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_uchar.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_matrix_char.h:
+/scratch/schoenmann/MolflowLinux/include/include/gsl/gsl_sf_erf.h:
+/scratch/schoenmann/MolflowLinux/molflowlinux_sub/gsl/gsl_sf_result.h:
+../molflowlinux_sub/TruncatedGaussian/rtnorm.hpp:
--- Molflow10.tcsh
+++ Molflow10.tcsh
@@ -1,11 +1,11 @@
 module load mpi
 
 if (! $?LD_LIBRARY_PATH) then       
-  setenv LD_LIBRARY_PATH $HOME/MolflowLinux/include/lib
+  setenv LD_LIBRARY_PATH "$PWD"/include/lib
 else
   if ("$LD_LIBRARY_PATH" == "")  then
-      setenv LD_LIBRARY_PATH $HOME/MolflowLinux/include/lib
+      setenv LD_LIBRARY_PATH "$PWD"/include/lib
   else 
-      setenv LD_LIBRARY_PATH $HOME/MolflowLinux/include/lib\:$LD_LIBRARY_PATH
+      setenv LD_LIBRARY_PATH "$PWD"/include/lib\:$LD_LIBRARY_PATH
   endif
 endif
